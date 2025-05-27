4b6c7648554acab6c4633f08a86a1ae00f5ae390
Priya <priyay1104@gmail.com>
2025-05-27 09:39:10 +0530

    "All operations get,post,patch,delete done in server"

--- project_01/index.js
+++ project_01/index.js
@@ -1,10 +1,10 @@
 
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
 const users = require('./MOCK_DATA.json');
 
-const PORT = 8000
+const PORT = 8000;
 
 //MIDDLEWARE - PLUGIN
 app.use(express.urlencoded({extended: false}));
@@ -27,15 +27,45 @@ app.get('/api/users/:id', (req,res)=>{
     res.json(user);
 });
 
-
+//POST
 app.post('/api/users', (req,res)=>{
     const data = req.body;
-    users.push({id : users.length + 1,...data});
-    const fs = require('fs');
+    console.log("Data : ", data , " id: " , (users.length+1));
+    users.push({id : (users.length + 1),...data});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data)=>{
-        console.log("Error: " , err);
+        return res.json({ status : "success", id : users.length});
     });
-    fs.close();
+});
+
+
+//PATCH
+app.patch('/api/users/:id', (req, res)=>{
+    const id = Number(req.params.id);
+    const updates = req.body;
+    const updatedUser = {id : id , ...updates};
+    const finalUsers = users.map((user) => {
+        if(user.id == id){
+            return updatedUser;
+        }
+        else{
+            return user;
+        }
+    });
+    console.log(updatedUser);
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(finalUsers) , (err)=>{
+        return res.json({status : "success", updatesUser : updatedUser});
+    })
+});
+
+
+//DELETE
+app.delete('/api/users/:id',(req,res)=>{
+    const userId = req.params.id;
+    const newusers = users.filter((user)=>user.id!=userId);
+    // console.log("User deleted");
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(newusers),(err)=>{
+        return res.json({status : "success", "deletedUserId" : userId});
+    })
 });
 
 app.listen(PORT , ()=>console.log("Server Started!"));
