25f6a6cdd2a66ac06292bd94878e4f7f2709f337
Priya <priyay1104@gmail.com>
2025-06-11 11:30:31 +0530

    "Starting frontend of short_url"

--- Short_url/index.js
+++ Short_url/index.js
@@ -4,13 +4,14 @@ const {connectMongoDB} = require('./connect');
 const app = express();
 const PORT = 8000;
 const URL = require('./models/Url');
+
+
 app.use(express.urlencoded({extended: false}));
-
 app.use(express.json());
-
-
 app.use('/url',urlRouter);
 
+//setting view engine
+app.set("view engine", "ejs");
 
 connectMongoDB("mongodb://127.0.0.1:27017/short_url_db").then(()=>console.log("Database connected successfully"));
 
